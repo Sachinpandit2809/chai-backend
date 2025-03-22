4409deb1cb56635752df0567e8b7e15eb84d7594
Sachinpandit2809 <sachinpandit2485@gmail.com>
2025-03-22 10:06:13 +0530

    connection of mongodb

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// require("dotenv").config({path: './.env'});
+import dotenv from "dotenv";
+import express from "express";
+import dbConnect from "./db/db_connect.js";
+dotenv.config({path: './.env'});
+
+
+dbConnect();
+
+
+
+
+
+
+
+
+
+
+
+
+const app = express();
+/*
+;(async()=>{
+    try {
+    await    mongoose.connect(`${process.env.MONGODB_REMOTE_URI}/${DB_NAME}`,)
+      app.on("error", (error) => {
+        console.error("App not able to connect ",error);
+        throw error;
+      });
+        app.listen(process.env.PORT, () => {
+            console.log(`App is running on 4{process.env.PORT}`);  });
+    } catch (error) {
+        console.error("Error connecting to mongodb  ",error);
+        throw error;
+    }
+})
+
+*/
